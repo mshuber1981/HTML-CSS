fd654a0aefd81bbb5666bcee7de84b8d74b8b9f7
Michael Huber <michael.huber1981@outlook.com>
2021-07-10 09:58:06 -0500

    Updating comments

--- Backroads/app.js
+++ Backroads/app.js
@@ -1,37 +1,29 @@
-// ********** set date ************
-// select span
-const date = (document.getElementById(
-  "date"
-).innerHTML = new Date().getFullYear());
-
-// ********** nav toggle ************
-// select button and links
+/* Variables */
+const date = (document.getElementById("date").innerHTML =
+  new Date().getFullYear());
 const navBtn = document.getElementById("nav-toggle");
 const links = document.getElementById("nav-links");
-// add event listener
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+/* Event Listeners */
 navBtn.addEventListener("click", () => {
   links.classList.toggle("show-links");
 });
 
-// ********** smooth scroll ************
-// select links
-const scrollLinks = document.querySelectorAll(".scroll-link");
-scrollLinks.forEach(link => {
-  link.addEventListener("click", e => {
-    // prevent default
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
     e.preventDefault();
     links.classList.remove("show-links");
 
     const id = e.target.getAttribute("href").slice(1);
     const element = document.getElementById(id);
-    //
     let position = element.offsetTop - 62;
 
     window.scrollTo({
       left: 0,
       // top: element.offsetTop,
       top: position,
-      behavior: "smooth"
+      behavior: "smooth",
     });
   });
 });
