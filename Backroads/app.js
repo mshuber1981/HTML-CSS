907449d81b5dcd302f69ed7dc54dc5434f86f73d
mshuber1981 <michael.huber1981@outlook.com>
2021-07-18 17:22:48 -0500

    Updating comments

--- Backroads/app.js
+++ Backroads/app.js
@@ -8,17 +8,21 @@ const scrollLinks = document.querySelectorAll(".scroll-link");
 /* Event Listeners */
 navBtn.addEventListener("click", () => {
   links.classList.toggle("show-links");
-});
+}); // Show/hide nav links
 
+// Add Event Listener for all scrollLinks
 scrollLinks.forEach((link) => {
   link.addEventListener("click", (e) => {
     e.preventDefault();
+    // Show/hide nav links
     links.classList.remove("show-links");
 
+    // Get link ID
     const id = e.target.getAttribute("href").slice(1);
     const element = document.getElementById(id);
     let position = element.offsetTop - 62;
 
+    // Smooth scroll - https://developer.mozilla.org/en-US/docs/Web/API/Window/scrollTo
     window.scrollTo({
       left: 0,
       // top: element.offsetTop,
