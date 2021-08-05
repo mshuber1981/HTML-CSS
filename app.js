92f8d08dcf44c697741b09d255d75923fa3b8fa2
Michael Huber <michael.huber1981@outlook.com>
2021-08-05 13:48:09 -0500

    Adding Sidebar content/styles/functionality

---
+++ app.js
@@ -0,0 +1,26 @@
+/* Variables */
+const navbar = document.querySelector("#nav");
+const navBtn = document.querySelector("#nav-btn");
+const closeBtn = document.querySelector("#close-btn");
+const sidebar = document.querySelector("#sidebar");
+const date = document.querySelector("#date");
+
+// Set year
+// date.innerHTML = new Date().getFullYear();
+
+/* Event Listeners */
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 80) {
+    navbar.classList.add("navbar-fixed");
+  } else {
+    navbar.classList.remove("navbar-fixed");
+  }
+});
+
+navBtn.addEventListener("click", function () {
+  sidebar.classList.add("show-sidebar");
+}); // Show Sidebar
+
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+}); // Close Sidebar
